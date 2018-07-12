8ead8cffc06c100e6e186ba968755bad6dcbec7f
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2018-07-12 05:03:05 -0300

    pré-eject.

---
+++ src/components/Character/Item/index.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import Characters from '../__mocks__/characters'
+import Item from './index'
+
+describe('Character Item', () => {
+  it('Deve renderizar corretamente', () => {
+    const characterMock = Characters[0]
+    const wrapper = shallow(
+      <Item character={characterMock} name={characterMock.name} image={characterMock.thumbnail} />
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+})
